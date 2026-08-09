22a8718b7beafff67409c02e734ca0707387d19e
Isaac Mtsiriza <230100836@luanar.ac.mw>
2026-08-09 11:01:38 +0200

    Use the full BuyMesho gate at app entry

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import BuyMeshoGateLite from "./components/BuyMeshoGateLite";
+import BuyMeshoGate from "./components/BuyMeshoGate";
 import "./index.css";
 import { registerServiceWorker } from "./utils/offlineSyncManager";
 
@@ -8,6 +8,6 @@ registerServiceWorker();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BuyMeshoGateLite />
+    <BuyMeshoGate />
   </StrictMode>,
 );
