ddcd9ab3c98de2c0255545b1a2785e6d34aeaf00
Isaac Mtsiriza <230100836@luanar.ac.mw>
2026-08-09 10:17:42 +0200

    Use lightweight BuyMesho gate

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import BuyMeshoGate from './components/BuyMeshoGate.tsx';
+import BuyMeshoGateLite from './components/BuyMeshoGateLite.tsx';
 import './index.css';
 import { registerServiceWorker } from './utils/offlineSyncManager.ts';
 
@@ -8,6 +8,6 @@ registerServiceWorker();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BuyMeshoGate />
+    <BuyMeshoGateLite />
   </StrictMode>,
 );
