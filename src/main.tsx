ed786081a6acf0cf63a1b808a9f4c4ac346df064
Isaac Mtsiriza <230100836@luanar.ac.mw>
2026-08-09 10:45:03 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import BuyMeshoGateLite from './components/BuyMeshoGateLite.tsx';
-import './index.css';
-import { registerServiceWorker } from './utils/offlineSyncManager.ts';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import BuyMeshoGateLite from "./components/BuyMeshoGateLite";
+import "./index.css";
+import { registerServiceWorker } from "./utils/offlineSyncManager";
 
 registerServiceWorker();
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BuyMeshoGateLite />
   </StrictMode>,
