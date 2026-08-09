b0a05c4832e07e664e08f3916281d1ab7400d846
Isaac Mtsiriza <230100836@luanar.ac.mw>
2026-08-09 15:57:17 +0200

    Use centralized Firebase auth restoration before routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import BuyMeshoGate from "./components/BuyMeshoGate";
+import { ValidatorAuthProvider } from "./auth/ValidatorAuthProvider";
 import "./index.css";
 import { registerServiceWorker } from "./utils/offlineSyncManager";
 
@@ -8,6 +9,8 @@ registerServiceWorker();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BuyMeshoGate />
+    <ValidatorAuthProvider>
+      <BuyMeshoGate />
+    </ValidatorAuthProvider>
   </StrictMode>,
 );
