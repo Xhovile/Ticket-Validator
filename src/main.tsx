4c8320b80a0cfbdd6754300dad5a3eadf5463a27
Isaac Mtsiriza <230100836@luanar.ac.mw>
2026-08-10 22:57:48 +0200

    Allow public diagnostic route before auth gate

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import BuyMeshoGate from "./components/BuyMeshoGate";
+import DiagnosticPage from "./DiagnosticPage";
 import { ValidatorAuthProvider } from "./auth/ValidatorAuthProvider";
 import { AppErrorBoundary } from "./components/AppErrorBoundary";
 import "./index.css";
@@ -8,12 +9,18 @@ import { registerServiceWorker } from "./utils/offlineSyncManager";
 
 registerServiceWorker();
 
+const isDiagnosticRoute = window.location.pathname.replace(/\/+$/, "") === "/diagnostic";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AppErrorBoundary>
-      <ValidatorAuthProvider>
-        <BuyMeshoGate />
-      </ValidatorAuthProvider>
+      {isDiagnosticRoute ? (
+        <DiagnosticPage />
+      ) : (
+        <ValidatorAuthProvider>
+          <BuyMeshoGate />
+        </ValidatorAuthProvider>
+      )}
     </AppErrorBoundary>
   </StrictMode>,
 );
